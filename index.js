8811771b06e0e311c2f5e7a517e4ff577e9f8e13
JasonNaggles <JasonNaggles.verizon.net>
2023-08-23 12:25:37 -0400

    first commit

--- index.js
+++ index.js
@@ -1,61 +1,38 @@
-// Initiate the game state
+$(function() {
+  let currentPlayer = "X";
+  const cells = Array.from($(".cell"));
 
-let currentPlayer = "X";
-let cells = ["", "", "", "", "", "", "", "", ""];
-
-//Get the cells and reset button elements 
-let cellElements = document.querySelectorAll("td");
-let resetButton = document.getElementById("reset-button");
-
-//Add click event listeners to cells and reset button
-cellElements.forEach(function(cellElement, index) {
-    cellElement.addEventListener("click", function() {
-        if (cells[index] === "") {
-            cells[index] = currentPlayer;
-        if (checkWin()) {
-            alert(currentPlayer + " wins!");
-            resetGame();
-        } else if (checkTie()) {
-            alert("Tie game!");
+  cells.forEach(cell => {
+    $(cell).on("click", function() {
+      if (!$(cell).text()) {
+        $(cell).text(currentPlayer);
+        if (checkWin(currentPlayer)) {
+          alert(`${currentPlayer} wins!`);
+          resetGame();
+        } else if (cells.every(cell => $(cell).text())) {
+          alert("It's a draw!");
+          resetGame();
         } else {
-            currentPlayer = (currentPlayer === "X") ? "O" : "X";
+          currentPlayer = currentPlayer === "X" ? "O" : "X";
         }
-    }
-});
-});
-
-resetButton.addEventListener("click", resetGame);
-
-//Check if a player has won
-function checkWin() {
-    if ((cells[0] === currentPlayer && cells[1] === currentPlayer && cells[2] === currentPlayer) ||
-        (cells[3] === currentPlayer && cells[4] === currentPlayer && cells[5] === currentPlayer) ||
-        (cells[6] === currentPlayer && cells[7] === currentPlayer && cells[8] === currentPlayer) ||
-        (cells[0] === currentPlayer && cells[3] === currentPlayer && cells[6] === currentPlayer) ||
-        (cells[1] === currentPlayer && cells[4] === currentPlayer && cells[7] === currentPlayer) ||
-        (cells[2] === currentPlayer && cells[5] === currentPlayer && cells[8] === currentPlayer) ||
-        (cells[0] === currentPlayer && cells[4] === currentPlayer && cells[8] === currentPlayer) ||
-        (cells[2] === currentPlayer && cells[4] === currentPlayer && cells[6] === currentPlayer)) {
-        return true;
-        }
-        return false;
-        }
-
-//Check if the game is a tie
-function checkTie() {
-    for (let i = 0; i < cells.length; i++) {
-        if (cells[i] === "") {
-            return false;
-        }
-    }
-    return true;
-}
-
-//Reset game
-function resetGame() {
-    currentPlayer = "X";
-    cells = ["", "", "", "", "", "", "", "", ""];
-    cellElements.forEach(function(cellElement) {
-        cellElement.textContent = "";
+      }
     });
-}
+  });
+
+  function checkWin(player) {
+    const winningCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ];
+
+    return winningCombos.some(combo => {
+      return combo.every(index => $(cells[index]).text() === player);
+    });
+  }
+
+  function resetGame() {
+    cells.forEach(cell => $(cell).text(""));
+    currentPlayer = "X";
+  }
+});
