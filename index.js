907636fd267811787a316e9ab0e2dd2c35b3169c
JasonNaggles <JasonNaggles.verizon.net>
2023-08-23 21:55:12 -0400

    first commit

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-$(function() {
-  let currentPlayer = "X";
-  const cells = Array.from($(".cell"));
+const cells = $('#td');
+const resetButton = $('#resetButton');
+const displayMessage = $('#displayMessage');
+let currentPlayer = 'X';
+let gameActive = true;
 
-  cells.forEach(cell => {
-    $(cell).on("click", function() {
-      if (!$(cell).text()) {
-        $(cell).text(currentPlayer);
-        if (checkWin(currentPlayer)) {
-          alert(`${currentPlayer} wins!`);
-          resetGame();
-        } else if (cells.every(cell => $(cell).text())) {
-          alert("It's a draw!");
-          resetGame();
-        } else {
-          currentPlayer = currentPlayer === "X" ? "O" : "X";
-        }
-      }
-    });
-  });
+// Add click event listeners to grid cells
+cells.on('click', function() {
+    if (!$(this).text() && gameActive) {
+        $(this).text(currentPlayer);
+        checkWinner();
+        checkDraw();
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+});
 
-  function checkWin(player) {
+// Check for a winner
+function checkWinner() {
     const winningCombos = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8],
-      [0, 3, 6], [1, 4, 7], [2, 5, 8],
-      [0, 4, 8], [2, 4, 6]
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
     ];
 
-    return winningCombos.some(combo => {
-      return combo.every(index => $(cells[index]).text() === player);
-    });
-  }
+    for (const combo of winningCombos) {
+        const [a, b, c] = combo;
+        if (cells.eq(a).text() && cells.eq(a).text() === cells.eq(b).text() && cells.eq(a).text() === cells.eq(c).text()) {
+            gameActive = false;
+            highlightWinner(cells.eq(a), cells.eq(b), cells.eq(c));
+            displayMessage.text(`Player ${currentPlayer} wins!`);
+            break;
+        }
+    }
+}
 
-  function resetGame() {
-    cells.forEach(cell => $(cell).text(""));
-    currentPlayer = "X";
-  }
-});
+// Check for a draw
+function checkDraw() {
+    if (cells.toArray().every(cell => $(cell).text() !== '') && gameActive) {
+        gameActive = false;
+        displayMessage.text("It's a draw!");
+    }
+}
+
+// Highlight the winning combination
+function highlightWinner(cell1, cell2, cell3) {
+    cell1.addClass('winner');
+    cell2.addClass('winner');
+    cell3.addClass('winner');
+}
+
+// Reset the game
+resetButton.on('click', resetGame);
+
+function resetGame() {
+    cells.text('');
+    cells.removeClass('winner');
+    currentPlayer = 'X';
+    gameActive = true;
+    displayMessage.text('');
+}
