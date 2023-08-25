22fbf32d07b854b95708e6e67f85a3d2239489dd
JasonNaggles <JasonNaggles.verizon.net>
2023-08-25 11:53:42 -0400

    first commit

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ cells.forEach(cell => {
     if (!cell.textContent) {
       cell.textContent = currentPlayer;
       if (checkWin(currentPlayer)) {
-        alert(`${currentPlayer} wins!`);
+        alert(`${currentPlayer} wins!`); //If there is a winner, a pop-up window all appear saying either "X's wins" or "O's wins" and the restart the game 
         resetGame();
       } else if (Array.from(cells).every(cell => cell.textContent)) {
-        alert("It's a draw!");
+        alert("It's a draw!"); // If there is a draw, a pop-up window will appear saying "It's a draw" and then restart the game
         resetGame();
       } else {
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
@@ -17,8 +17,8 @@ cells.forEach(cell => {
     }
   });
 });
-resetButton.addEventListener('click', resetGame);
-
+resetButton.addEventListener('click', resetGame); // Gives the user the options to reset the game when the tic=tac-toe board is not completely filled out
+// This function checks for the winner by presenting 3 X's or O's vertically, horizontally, or diagonally.
 function checkWin(player) {
   const winCombos = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
